663abcef278c96305d6d7661ef68ef354a2d538a
piyushxp <piyushmahapatra001@gmail.com>
2021-07-21 13:34:15 +0530

    decorator-js

---
+++ js-decorators/main.js
@@ -0,0 +1,27 @@
+// Decorator wrap a function in another function
+// These wrappers 'decorate' the original function
+// Benefits --> DRY and clean code through Compositions
+
+// Javascript Decorator Functions
+let sum = (...args) => {
+	return [...args].reduce((acc, curr) => acc + curr);
+};
+
+//callCounter is a decorator , going to count how many time a fn is called
+const callCounter = (fn) => {
+	let count = 0;
+
+	//using closure we have access to the count varaible inside the anonymous function
+	return (...args) => {
+		console.log(`Sum has been called ${(count += 1)} times`);
+		return fn(...args);
+	};
+};
+
+//wrap the original fn
+sum = callCounter(sum);
+
+//Call
+console.log(sum(1, 2, 3)); //Sum has been called 1 times
+console.log(sum(3, 4, 5)); //Sum has been called 2 times
+console.log(sum(6, 7, 8)); //Sum has been called 3 times
